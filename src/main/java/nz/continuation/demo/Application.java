fb9037c168fb87f45ff31a0712a4dd95532d6a76
Julian Simpson <simpsonjulian@gmail.com>
2022-06-04 17:42:53 +1200

    Working Spring Boot Demo application

---
+++ src/main/java/nz/continuation/demo/Application.java
@@ -0,0 +1,20 @@
+package nz.continuation.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    @GetMapping("/")
+    public String home() {
+        return "This is a Spring Boot app on Docker";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
